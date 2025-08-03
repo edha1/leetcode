a2130e9a1378f7e390a996d668a7a7c3f61ed22f
edha1 <edha.misra@gmail.com>
2025-08-03 13:59:10 +0100

    added 2125 and 2733

---
+++ solutions/number_of_laser_beams_2125.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def numberOfBeams(self, bank):
+        """
+        :type bank: List[str]
+        :rtype: int
+        """
+
+        # for the top row, we can mark the column as a laser beam. 
+        # then for intermediate lazers
+
+        lasers = 0 
+        prev_level_count = 0 
+
+        # check where the lasers are 
+        for i in range(len(bank)): 
+            level_count = 0 
+            for j in range(len(bank[0])): 
+                if bank[i][j] == "1": 
+                    level_count += 1 
+
+            if level_count != 0: 
+                # the number of lasers is this formula 
+                lasers += (prev_level_count * level_count) 
+
+                # update for the next level 
+                prev_level_count = level_count 
+    
+        return lasers 
+
+
+        
